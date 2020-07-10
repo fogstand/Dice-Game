3d65e61fc2fdfe43b209cb6dd557987f078811ad
ravishersingh1 <60622194+ravishersingh1@users.noreply.github.com>
2020-07-10 17:16:41 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+
+  
+
+
+  const [firstDieResult,setFirstDieResult] = useState(1);
+  const [secondDieResult, setSecondDieResult] = useState(6);
+
+  const firstDieImage = require(`./assets/${firstDieResult}.png`);
+  const secondDieImage = require(`./assets/${secondDieResult}.png`);
+
+  function rollDice(){
+    setFirstDieResult(Math.floor(Math.random()*6)+1);
+    setSecondDieResult(Math.floor(Math.random()*6)+1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ="App">
+      <header className = "App-header">
+        <div style= {{ display: 'flex', margin:20}}>
+         <img src ={firstDieImage} className = "die" alt = "Die one" />
+         <img src={secondDieImage} className = "die" alt = "Die two" />
+
+
+
+        </div>
+        <span>{firstDieResult + secondDieResult} </span>
+        <button className= "button" onClick={rollDice}>Roll</button>
+        </header>
+
+
+
+
+  
     </div>
   );
 }
